69e09a1a0835dfe7d8f170b127356f3fc1d8a903
Dato88 <66302751+Dato88@users.noreply.github.com>
2020-06-25 19:17:30 +0200

    Movementscript ver 0.4

---
+++ 04 AnNiMe Projekt Lava/Assets/Scripts/characterMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float forwardVel = 12;
+        public float rotateVel = 100;
+        public float JumpVel = 25;
+        public float distToGrounded = 0.1f;         //vielleicht auf .5 .8 setzen
+        public LayerMask ground;
+    }
+
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downAccel = 0.75f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;     //für einen kleinen Delay beim Drücken einer Taste
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";                       //bei 7:20
+    }
+
+    public MoveSettings moveSetting = new MoveSettings();
+    public PhysSettings physSettings = new PhysSettings();
+    public InputSettings inputSettings = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;      //normale werte sind dann 1 und 0
+
+    public Quaternion TargetRotation    
+    {
+        get { return targetRotation; }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distToGrounded, moveSetting.ground);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>()) {
+            rBody = GetComponent<Rigidbody>();
+        } else {
+            Debug.LogError("The charactor needs a rigidbody!!");    //Falls kein Rigidbody vorhanden ist
+        }
+
+        forwardInput = turnInput = 0;                               //erste initialisierung der Variablen
+
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Turn();
+
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            //move
+            rBody.velocity = transform.forward * forwardInput * forwardVel;     //forwardInput bestimmt ob vorwärts oder rückwärts gegangen wird
+        } else
+        {
+            //zero velocity
+            rBody.velocity = Vector3.zero;
+        }
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+}
