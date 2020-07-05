f3b551274b41a2c68e12d24de410b31db3f21280
NinaRup <67108459+NinaRup@users.noreply.github.com>
2020-07-05 13:49:44 +0200

    Cupe hoch erstellt

--- 04 AnNiMe Projekt Lava/Assets/Scripts/NR_platte.cs
+++ 04 AnNiMe Projekt Lava/Assets/Scripts/NR_platte.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class NR_platte : MonoBehaviour
 {
+
+    GameObject platte;
+    private float time = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //platte = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        platte = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        platte.transform.localScale = new Vector3(1,1,1);
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time >= time)
+        {           
+        time += 1.0f;
+        platte.transform.position += platte.transform.localRotation * new Vector3(0f, 1.0f, 0);
+        }    
     }
 }
