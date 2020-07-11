a2cd4aad14a0d8a9d382c8a84e42fcc88e9c7c05
Melirai <53022575+Melirai@users.noreply.github.com>
2020-07-11 13:38:08 +0200

    lavakugeln

---
+++ 04 AnNiMe Projekt Lava/Assets/lavakugeln.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lavakugeln : MonoBehaviour
+{
+    public GameObject lavakugel;
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        ground.transform.localScale = new Vector3(-10.0f*2,0.1f,10.0f*2);
+        ground.transform.Translate(-3,0,-3);
+
+        lavakugel = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       int x = 0;
+       int time = 0;
+       float speed = 0.1f;
+
+        while(time >= x && time <= x+2)
+        {
+            lavakugel.transform.position += Vector3.up * speed;
+            x++
+        }
+
+        
+    }
+}
