75229387581f63b8818a2a9b8c687f77d23eba1a
Melirai <53022575+Melirai@users.noreply.github.com>
2020-07-11 15:42:44 +0200

    lavakugeln

--- 04 AnNiMe Projekt Lava/Assets/Scripts/lavakugeln.cs
+++ 04 AnNiMe Projekt Lava/Assets/Scripts/lavakugeln.cs
@@ -6,10 +6,15 @@ public class lavakugeln : MonoBehaviour
 {
     public GameObject lavakugel;
     public Collider groundCollider;
+    Rigidbody rig;
 
     // Start is called before the first frame update
     void Start()
     {
+        lavakugel = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        rig = lavakugel.AddComponent<Rigidbody>();
+        rig.isKinematic = false;
+
         //Boden
         GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Cube);
         ground.name = "ground";
@@ -21,16 +26,17 @@ public class lavakugeln : MonoBehaviour
     void Update()
     {
        float waittime = 20.0f;
-       float speed = 0.1f;
+       float speed = 1.0f;
        float time = Time.deltaTime;
 
-       lavakugel = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
-        while(waittime > 0.0f)
+        lavakugel.transform.position += Vector3.up * speed;
+       // if(lavakugel.position)
+        Debug.Log(time);
+        if(time == 6.0f)
         {
-            lavakugel.transform.position += Vector3.up * speed;
-            waittime--;
+            rig.isKinematic = true;
         }
+    
 
 
 
